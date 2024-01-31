94fcabbb89f2a039fd75724adfc3d16825295237
Deepali Rai <deepali.rai@dianapps.com>
2024-01-31 10:15:57 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+            <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+//       <div className="w-full flex-none md:w-64">
+//         <SideNav />
+//       </div>
+//       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+//     </div>
+//     </html>
+//   );
+// }
